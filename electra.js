1b6085e01c83cb498be52f0cea192570b38cb79d
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2021-05-09 23:42:10 +1000

    Update electra.js
    Optimize code, remove redundant switch case statement, and provide exporting to each of the imported 'devices' within the electra.js module.

---
+++ electra.js
@@ -0,0 +1,12 @@
+/**
+ * Module dependencies.
+ */
+ var generator = require('./lib/devices/generator');
+ var grid = require('./lib/devices/grid');
+ var localcontroller = require('./lib/devices/localcontroller');
+ var smartswitch = require('./lib/devices/smartswitch');
+ var storage = require('./lib/devices/storage');
+ 
+module.exports = {
+    generator
+};
