6711199354e0a8b36eb22d0b0b6b29f0dcc7a9d9
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2021-05-11 00:51:55 +1000

    Update electra.js

--- electra.js
+++ electra.js
@@ -1,12 +1,15 @@
 /**
  * Module dependencies.
  */
- var generator = require('./lib/devices/generator');
+ var generator = require('./lib/devices/generator').generator;
  var grid = require('./lib/devices/grid');
- var localcontroller = require('./lib/devices/localcontroller');
+ var localcontroller = require('./lib/devices/localcontroller').localcontroller;
  var smartswitch = require('./lib/devices/smartswitch');
  var storage = require('./lib/devices/storage');
- 
-module.exports = {
-    generator
-};
+
+const electra = {
+    generator: generator,
+    localcontroller: localcontroller
+}
+
+module.exports = electra;
