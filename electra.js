d45f15f5d6ab6c8b70d83dad5f8fc7b090e90fdd
arytek <38933061+arytek@users.noreply.github.com>
2021-12-16 23:57:24 +1100

    Export grid and smartswitch device libraries.
    Adds 'grid.js' and 'smartswitch.js' device libraries to the export list.

--- electra.js
+++ electra.js
@@ -2,15 +2,17 @@
  * Module dependencies.
  */
  var generator = require('./lib/devices/generator').generator;
- var grid = require('./lib/devices/grid');
+ var grid = require('./lib/devices/grid').grid;
  var localcontroller = require('./lib/devices/localcontroller').localcontroller;
- var smartswitch = require('./lib/devices/smartswitch');
+ var smartswitch = require('./lib/devices/smartswitch').smartswitch;
  var storage = require('./lib/devices/storage').storage;
 
 const electra = {
     generator: generator,
     localcontroller: localcontroller,
-    storage: storage
+    storage: storage, 
+    smartswitch: smartswitch,
+    grid: grid
 }
 
 module.exports = electra;
